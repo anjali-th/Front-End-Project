a1bd09b2f5f37cc6ba69f1294f855428c5e03c59
anjali-th <anjalithing@gmail.com>
2022-02-16 19:25:09 -0600

    End of Day1

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 const $body=$('body');
 
 const $divMain= $('<div></div>');
-$divMain.addClass('main');
+$divMain.addClass('container-xxl text-center');
 $divMain.html('<h1>University Search</h1><br><h5>Enter a country</h5>');
 $body.append($divMain);
 
 const $divInput = $('<div></div>');
 $divInput.addClass('userInput');
 $divInput.html("<input></input>")
-$body.append($divInput);
+$divMain.append($divInput);
 
 const $btn=$('<button>Search</button>');
 const $btnClr=$('<button>Clear</button>');
@@ -20,12 +20,11 @@ let resultData;
 
 // ajax request
 const get = function(){
-    $.get(`http://universities.hipolabs.com/search?name=${$('input').val()}`, (data) => {
-    resultData = data;
-    createList(resultData);
+    $.get(`http://universities.hipolabs.com/search`, (data) => {
+        resultData = data;
+        createList(resultData);
     });
 }
-
 // calling ajax request
 $btn.on('click', () =>{
     get();
@@ -44,16 +43,25 @@ $btnClr.on('click', () =>{ window.location.reload()});
 function createList(){
 
     const $listContainer = $('<div></div>');
+    $listContainer.addClass('container-sm')
     $listContainer.insertAfter($btnClr);
    
     for ( var index = 0; index<resultData.length; index++){
+
+        if (resultData[index].country == $('input').val()){
     
         $(`<div>${resultData[index].name}</div>`).appendTo($listContainer);
         $(`<div><a href= ${resultData[index].web_pages}>${resultData[index].web_pages}</a></div>`).appendTo($listContainer);
-        $('a').on('click', () => { window.open($('a').prop('href')) });
-        
+        $('a').on('click', () => { window.open($('a').prop('href'), '_blank')});
+
+        };
     };
 };
+        
+  
+    
+
+
 
 
 
