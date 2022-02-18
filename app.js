a689a04e18ccfd3e446037bceacdd4812fe2b74e
anjali-th <anjalithing@gmail.com>
2022-02-17 21:41:51 -0600

    Day 2 commit 1

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
+//US colleges
 
-const $body=$('body');
 
-const $divMain= $('<div></div>');
-$divMain.addClass('container-xxl text-center');
-$divMain.html('<h1>University Search</h1><br><h5>Enter a country</h5>');
-$body.append($divMain);
 
-const $divInput = $('<div></div>');
-$divInput.addClass('userInput');
-$divInput.html("<input></input>")
-$divMain.append($divInput);
 
-const $btn=$('<button>Search</button>');
-const $btnClr=$('<button>Clear</button>');
-$divInput.append($btn);
-$divInput.append($btnClr);
+
+
+
+
+
+//Outside US
+const $divWorld= $('#world-div');
+const $inputWrld=$('#world-input');
+const $btnWrld=$("#world-search");
+const $btnClrWrld=$("#world-clear");
 
 let resultData;
 
@@ -26,56 +24,33 @@ const get = function(){
     });
 }
 // calling ajax request
-$btn.on('click', () =>{
+$btnWrld.on('click', () =>{
     get();
 })
 
-$('input').on('keypress', (e) =>{
+$inputWrld.on('keypress', (e) =>{
     if (e.key === 'Enter'){
         get();
     }
 })
     
 // clear button
-$btnClr.on('click', () =>{ window.location.reload()});
+$btnClrWrld.on('click', () =>{ window.location.reload()});
 
 // create list of Universities
 function createList(){
 
     const $listContainer = $('<div></div>');
-    $listContainer.addClass('container-sm')
-    $listContainer.insertAfter($btnClr);
-   
+    $listContainer.addClass('container-xxl');
+    $listContainer.appendTo($divWorld);
+
     for ( var index = 0; index<resultData.length; index++){
-
-        if (resultData[index].country == $('input').val()){
-    
-        $(`<div>${resultData[index].name}</div>`).appendTo($listContainer);
-        $(`<div><a href= ${resultData[index].web_pages}>${resultData[index].web_pages}</a></div>`).appendTo($listContainer);
-        $('a').on('click', () => { window.open($('a').prop('href'), '_blank')});
-
+        if (resultData[index].country == $inputWrld.val()){
+            $(`<div>${resultData[index].name}</div>`).appendTo($listContainer);
+            $(`<div><a href= ${resultData[index].web_pages}>${resultData[index].web_pages}</a></div>`).appendTo($listContainer);
+            $('a').on('click', () => { window.open($('a').prop('href'))});
         };
     };
 };
-        
-  
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
    
-    
-
-    
