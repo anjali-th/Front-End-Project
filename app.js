7c6044449285266558f8c2445f28d806a8347096
anjali-th <anjalithing@gmail.com>
2022-02-17 23:22:05 -0600

    Day 2 commit 2

--- app.js
+++ app.js
@@ -1,11 +1,70 @@
 //US colleges
+let resultDataUs;
+const $divUs=$('#us-search-div');
+const $inputUs=$('#us-input');
+const $btnClrUs=$('#us-clear');
+const $btnUs=$('#us-search')
 
 
+//ajax request
+const getUs = function(){
+    let apiKey= "&api_key=UNJxyTeSmB2pxVOaLOkXBon7vjjsKM6OT57LMfen"
+    $.get(`https://api.data.gov/ed/collegescorecard/v1/schools?school.name=${$inputUs.val()}${apiKey}`, (data) => {
+        resultDataUs = data;
+        console.log(resultDataUs)
+        createCard(resultDataUs);
+    });
+}
+
+//calling ajax request 
+$btnUs.on('click', () =>{
+    getUs();
+});
+
+$inputUs.on('keypress', (e) =>{
+    if (e.key === 'Enter'){
+        getUs();
+    }
+});
+   
+// clear btn
+$btnClrUs.on('click', () =>{ window.location.reload()});
+
+//create university card
+function createCard(){
+
+    let info = resultDataUs.results;
+    
+    const $uniCard=$('<div></div>');
+    $uniCard.addClass('card');
+    $uniCard.appendTo($divUs);
+    
+    for ( let indexUs = 0; indexUs < info.length; indexUs++){
+        
+        const $ul=$('<ul></ul>');
+        $ul.addClass('list');
+        $ul.appendTo($uniCard);
+        
+        let acceptance= info[indexUs].latest.admissions.admission_rate.overall * 100 ;
+        
+        $ul.html(
+            `<li class="list-group-item">${info[indexUs].latest.school.name}</li>
+            <li class="list-group-item">City: ${info[indexUs].latest.school.city}, ${info[indexUs].latest.school.state} ${info[indexUs].school.zip}</li>
+            <li class="list-group-item">Acceptance rate: ${acceptance}%</li>
+            <li class="list-group-item">In-state Tuition: $${info[indexUs].latest.cost.tuition.in_state}</li>
+            <li class="list-group-item">Out-of-State Tuition: $${info[indexUs].latest.cost.tuition.out_of_state}</li>
+            <li class="list-group-item">Accreditation: ${info[indexUs].latest.school.accreditor}</li>
+            <li class="list-group-item">Webite: <a href= ${info[indexUs].latest.school.school_url}>${info[indexUs].latest.school.school_url}</a> </li>
+            <li class="list-group-item">Price Calculator: <a href= ${info[indexUs].latest.school.price_calculator_url}>${info[indexUs].latest.school.price_calculator_url}</a></li>`
+        );
+    }
+    
 
 
+}
 
 
-
+   
 
 
 //Outside US
